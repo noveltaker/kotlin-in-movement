cb0bbd749aea025f1efe2c24e471aa4c27fe95df
gonzo <m165252@daum.net>
2021-08-07 11:12:44 +0900

    string template

---
+++ src/base/string.kt
@@ -0,0 +1,8 @@
+package base
+
+fun main(args: Array<String>) {
+    var name  : String = "Hello"
+    println("test sucess")
+    println("test sucess $name")
+    println("test sucess ${name}")
+}
