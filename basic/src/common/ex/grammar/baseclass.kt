70c726e5616b730d4c9b512fb6d3e43e0dbcb7cd
gon-zo <pnw123456@icloud.com>
2021-10-23 09:57:15 +0900

    chore:remove project

--- basic/src/common/ex/grammar/baseclass.kt
+++
@@ -1,9 +0,0 @@
-package base
-
-class Person(val str: String)
-
-
-fun main(args: Array<String>) {
-    val person = Person("Test...")
-    println(person.str)
-}
--- basic/src/common/ex/grammar/string.kt
+++
@@ -1,8 +0,0 @@
-package base
-
-fun main(args: Array<String>) {
-    var name  : String = "Hello"
-    println("test sucess")
-    println("test sucess $name")
-    println("test sucess ${name}")
-}
