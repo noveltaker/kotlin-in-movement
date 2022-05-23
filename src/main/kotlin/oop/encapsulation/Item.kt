a1743a6344d78ac9fa7161e068f9d51d52877bb7
noveltaker <53357210+noveltaker@users.noreply.github.com>
2022-05-23 13:10:01 +0900

    Delete src/main/kotlin/oop directory

--- src/main/kotlin/oop/encapsulation/Item.kt
+++
@@ -1,8 +0,0 @@
-package oop.encapsulation
-
-class Item(private val content: String, private val name: String) {
-
-    fun getItemName() = this.name
-
-    fun getItemContent() = this.content
-}
--- src/main/kotlin/oop/encapsulation/run.kt
+++
@@ -1,7 +0,0 @@
-package oop.encapsulation
-
-fun main(args: Array<String>) {
-    val item = Item("컨텐츠", "아이템")
-    println(item.getItemName())
-    println(item.getItemContent())
-}
